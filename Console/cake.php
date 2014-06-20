01303a7ca8911e3424fa6dd72d81bdb6e7a96c82
Rachman Chavik <rchavik@xintesa.com>
2014-06-20 18:51:12 +0700

    Sync DS similar to changes in upstream
    See cakephp/cakephp@a9de83daeb14134

--- Console/cake.php
+++ Console/cake.php
@@ -17,37 +17,40 @@
  * @since         CakePHP(tm) v 2.0
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
-$ds = DIRECTORY_SEPARATOR;
-$dispatcher = 'Cake' . $ds . 'Console' . $ds . 'ShellDispatcher.php';
+if (!defined('DS')) {
+		define('DS', DIRECTORY_SEPARATOR);
+}
+
+$dispatcher = 'Cake' . DS . 'Console' . DS . 'ShellDispatcher.php';
 $found = false;
 $paths = explode(PATH_SEPARATOR, ini_get('include_path'));
 
 foreach ($paths as $path) {
-	if (file_exists($path . $ds . $dispatcher)) {
+	if (file_exists($path . DS . $dispatcher)) {
 		$found = $path;
 	}
 }
 
 if (!$found && function_exists('ini_set')) {
 	$appDir = dirname(dirname(__FILE__));
-	$vendorDir = $appDir . $ds . 'Vendor' . $ds;
+	$vendorDir = $appDir . DS . 'Vendor' . DS;
 	$root = dirname($appDir);
 	ini_set('include_path',
-		$root . $ds . 'lib' . PATH_SEPARATOR .
-		$vendorDir . 'cakephp' . $ds . 'cakephp' . $ds . 'lib' . PATH_SEPARATOR .
+		$root . DS . 'lib' . PATH_SEPARATOR .
+		$vendorDir . 'cakephp' . DS . 'cakephp' . DS . 'lib' . PATH_SEPARATOR .
 		ini_get('include_path')
 	);
 
 	if (in_array('test', $argv) && in_array('croogo', $argv) && !defined('TESTS')) {
 		$key = array_keys($argv, 'croogo');
 		$argv[current($key)] = 'app';
-		define('TESTS', $vendorDir . 'croogo' . $ds . 'croogo' . $ds . 'Test' . $ds);
+		define('TESTS', $vendorDir . 'croogo' . DS . 'croogo' . DS . 'Test' . DS);
 	}
 }
 
 if (!include ($dispatcher)) {
 	trigger_error('Could not locate CakePHP core files.', E_USER_ERROR);
 }
-unset($paths, $path, $found, $dispatcher, $root, $ds);
+unset($paths, $path, $found, $dispatcher, $root);
 
 return ShellDispatcher::run($argv);
